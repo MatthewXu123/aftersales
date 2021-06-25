44d08877293b12bbabe4bb3cabafaf8fa4044684
Matthew Xu <matthew.xu@carel.locale>
2021-06-25 10:39:37 +0800

    0625

--- src/test/java/com/carel/repository/SparePartRepositoryTest.java
+++ src/test/java/com/carel/repository/SparePartRepositoryTest.java
@@ -4,9 +4,7 @@ package com.carel.repository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.carel.persistence.entity.pk.SparePartPK;
@@ -18,8 +16,7 @@ import com.carel.persistence.entity.product.SparePart;
  * @date Jun 24, 2021
  */
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@SpringBootTest
 public class SparePartRepositoryTest {
 
 	@Autowired
